f5200a46190905fa682ac1a69b8a82fff746c41d
Lucas Andersen <lucas@e-vision.dk>
2023-02-14 10:49:02 +0100

    code

--- ordination/Ordination.java
+++ ordination/Ordination.java
@@ -6,14 +6,16 @@ import java.time.temporal.ChronoUnit;
 public abstract class Ordination {
     private LocalDate startDen;
     private LocalDate slutDen;
+    private Patient patient;
 
     // TODO Link til Laegemiddel
     private Laegemiddel laegemiddel;
 
     // TODO constructor (med specifikation)
-    public Ordination(LocalDate startDen, LocalDate slutDen) {
+    public Ordination(LocalDate startDen, LocalDate slutDen, Patient patient) {
         this.startDen = startDen;
         this.slutDen = slutDen;
+        this.patient = patient;
     }
 
     public LocalDate getStartDen() {
